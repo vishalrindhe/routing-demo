5d4a550b7fc70b2cf160e8db9dbd529b80a04bf7
servicefirstexp <rindhevishal32@gmail.com>
2020-05-12 02:53:36 +0530

    wildcard route and redirect route: video 24

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DeptlistComponent } from './deptlist/deptlist.component';
 import { EmplistComponent } from './emplist/emplist.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+  // { path: '', component: DeptlistComponent }, //this is empt path scenario like default 
+  { path: '', redirectTo: '/departments', pathMatch: 'full' }, 
+  // for aboves line explaination 
+  //if  URL is "Full" empt then onl it will redirect  to the "departments"
+  // we tried "prefix" as pathmatch also but it redirect to "department" for all pages and URL
+
   { path: 'departments', component: DeptlistComponent },
-  { path: 'employees', component: EmplistComponent }
+  { path: 'employees', component: EmplistComponent },
+
+  // this is wildcard route. It should be alwas last in the paths.
+  // if an route not found then it will show this "page not found"
+  // if wildcard route placed at the first line it will automaticall show "pag not found " for all url
+  { path: "**", component: PageNotFoundComponent }
 
 ];
 
